17ae7133403f797f3c0da42feefd53760a754d45
Daniel Levett <dlevett@hotmail.co.uk>
2021-12-27 23:20:29 +0000

    Tidying

--- src/LinkDefinitionProvider.ts
+++ src/LinkDefinitionProvider.ts
@@ -1,11 +1,28 @@
 import * as vscode from "vscode";
-import { getLinks } from "./getLinks";
 
 export class LinkDefinitionProvider implements vscode.DocumentLinkProvider {
   public provideDocumentLinks(
-		document: vscode.TextDocument,
-		token: vscode.CancellationToken
-	): vscode.ProviderResult<vscode.DocumentLink[]> {
-		return getLinks(document);
-	}
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.DocumentLink[]> {
+    const regEx = /FOO-\d+/g;
+    const text = document.getText();
+    const links: vscode.DecorationOptions[] = [];
+
+    let match;
+    while ((match = regEx.exec(text))) {
+      const startPos = document.positionAt(match.index);
+      const endPos = document.positionAt(match.index + match[0].length);
+      const decoration: vscode.DocumentLink = {
+        range: new vscode.Range(startPos, endPos),
+        target: vscode.Uri.parse(
+          `https://www.google.com/search?q=${match[0]}`,
+          true
+        ),
+      };
+      links.push(decoration);
+    }
+
+    return links;
+  }
 }
